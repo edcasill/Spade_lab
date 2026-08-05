edb834673e398c79b8d4f73966576b4326b943fc
edcasill <acasillasval@gmail.com>
2026-08-05 12:20:48 -0600

    added basic agent file

---
+++ main.py
@@ -0,0 +1,34 @@
+import asyncio
+from spade_bdi.bdi import BDIAgent
+
+
+def main():
+    # agent with JID, password, AgentSpeak file
+    agent = BDIAgent("ranger@yourserver.com", "p@tr0ll", "behaviour_test.asl")
+
+    # initialice agent
+    await agent.start()
+
+    print("Agent connected to the net. Excecuting BDI reasoning")
+
+    # this can set beliefs to the agent without using the speak file
+    agent.bdi.set_belief("Temperature", 20)
+
+    # this retrieve the beliefs of the agent
+    """
+    curren_believe = agent.bdi.get_belief("battery_state")
+    all_beliefs = agent.bdi.get_beliefs()
+    """
+
+    # we can remove dinamically the beliefs of the agent
+    # agent.bdi.remove_belief("Temperature")
+
+    # this keeps alive the process, so teh agent has time to reason and execute an action
+    await asyncio.sleep(5)
+
+    # stop the agent
+    await agent.stop()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
